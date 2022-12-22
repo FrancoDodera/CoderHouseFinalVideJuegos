24098e52d9fbea2390577c922095984315f4d23d
Eze Capretta <caprettaeze@gmail.com>
2022-12-21 21:03:00 -0300

    movimientos personaje

---
+++ Flappy Ball/Assets/Scripts/ballMovement.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float doubleJumpSpeed;
+    public Rigidbody rb;
+    private bool m_isGrounded;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+    private bool canDoubleJump;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        m_isGrounded = true;
+    }
+    private void Update()
+    {
+        Jump();
+    }
+    void FixedUpdate()
+    {
+        CharacterMovement();
+    }
+    void CharacterMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 vector = new Vector3(horizontal,0,0);
+        rb.AddForce(vector * speed * Time.deltaTime);
+
+    }
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(m_isGrounded == true)
+            {
+                rb.AddForce(0, jumpSpeed, 0, ForceMode.Impulse);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    if(canDoubleJump)
+                    {
+                        rb.AddForce(0, doubleJumpSpeed, 0, ForceMode.Impulse);
+                        canDoubleJump = false;
+                    }
+                }
+            }
+            m_isGrounded = false;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("floor"))
+        {
+            m_isGrounded = true;
+        }
+    }
+}
