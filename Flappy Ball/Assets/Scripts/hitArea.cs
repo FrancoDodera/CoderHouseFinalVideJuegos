5e6272da070ead8583ddec414bd0ec057257d8f2
Eze Capretta <caprettaeze@gmail.com>
2022-12-24 15:35:22 -0300

    enemys scripting

---
+++ Flappy Ball/Assets/Scripts/hitArea.cs
@@ -0,0 +1,22 @@
+// using UnityEngine;
+
+// public class hitArea : MonoBehaviour
+// {
+//     public GameObject hit;
+//     public float timer;
+//     float damage = 1.5f;
+
+    
+//     void OnTriggerStay(Collider col)
+//     {
+//         if (col.CompareTag("Player"))
+//         {
+//             timer += Time.deltaTime;
+//             if(timer >= damage)
+//             {
+//                 Debug.Log("ahora si?");
+//                 Destroy(gameObject);
+//             }
+//         }
+//     }
+// }
